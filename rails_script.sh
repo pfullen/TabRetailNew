e6d3a1df5cbdd9d0d427f4787246ba72150fd2d6
Peter Fullen <peter@fullencpa.com>
2017-07-17 12:27:28 -0400

    implemented the ability to delete posts

---
+++ rails_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+alias s='rails s';
+alias b="bundle install";
+alias m="bundle exec rake db:migrate";
+alias setup="bundle exec rake db:setup";
+alias c='rails c';
+alias rg='rails g';
+alias rgr='rails g resource';
+alias rgm='rails g migration';
+alias rgmd='rails g model';
+
