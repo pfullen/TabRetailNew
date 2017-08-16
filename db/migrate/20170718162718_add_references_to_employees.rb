8619326b0eb0f9ad5d882f0894ea1a198a5c4e1f
Peter Fullen <peter@fullencpa.com>
2017-08-15 20:57:04 -0400

    Correct migration

--- db/migrate/20170718162718_add_references_to_employees.rb
+++
@@ -1,6 +0,0 @@
-class AddReferencesToEmployees < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :employees, :shift, foreign_key: true
-    add_reference :employees, :roster, foreign_key: true
-  end
-end
