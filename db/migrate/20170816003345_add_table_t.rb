10f4231ba3878001602c69504814024ae6a1993b
Peter Fullen <peter@fullencpa.com>
2017-08-15 20:45:49 -0400

    Correct migration

--- db/migrate/20170816003345_add_table_t.rb
+++ db/migrate/20170816003345_add_table_t.rb
@@ -1,6 +1,6 @@
 class AddTableT < ActiveRecord::Migration[5.0]
    def change
-    create_table :contacts do |t|
+    create_table :employees do |t|
       t.string :first_name
       t.string :last_name
       t.string :compnay
